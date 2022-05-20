ec2eac2a6b8e1f5399439188393f28b5f9fa29e3
Guilherme Felipe Campos <gui_fc@hotmail.com>
2022-05-20 17:11:53 -0300

    first commit

---
+++ src/main/java/br/com/desafio/testgradle/TestGradle/ModelViews/Home.java
@@ -0,0 +1,7 @@
+package br.com.desafio.testgradle.TestGradle.ModelViews;
+
+public class Home {
+  public String getMensagem(){
+    return "Aplicativo de criacão de tabelas utilizando o LiquiBase e Gradle";
+  }
+}
